81c200982ae46f61e405892e6e2cfb6e4d8cff60
shreyanshcr007 <divy.shreyansh@gmail.com>
2025-03-07 21:47:42 +0530

    First Commit

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+          Our company offers services like Cloud and Platform Modernization, SaaS Product and Platform Development, Cloud Native Application Development & Testing, Security as a Service (SECaaS), Cloud Security Engineering, and Cloud Governance & Reporting, ensuring scalable, secure, and efficient cloud solutions for modern businesses
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
