21796edea6095fb761e86fe510ff316b04ed1304
shreyanshcr007 <divy.shreyansh@gmail.com>
2025-03-07 22:25:11 +0530

    Updated New Company Name

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Ancrew Global Pvt. Ltd.
+            CloudNexa Technologies Pvt. Ltd.
           </a>{" "}
         </div>
 
